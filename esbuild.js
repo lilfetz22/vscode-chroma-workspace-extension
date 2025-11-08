b9fec5d54a01ca49ba0145caf8bb6f2e6b4b32b9
lilfetz22 <billfetzner@yahoo.com>
2025-11-08 07:35:15 -0500

    build(esbuild): add better-sqlite3 to external dependencies
    Mark better-sqlite3 as external to prevent bundling native module, allowing proper runtime loading from node_modules.

--- esbuild.js
+++ esbuild.js
@@ -13,7 +13,7 @@ async function main() {
     sourcesContent: false,
     platform: 'node',
     outfile: 'dist/extension_bundled.js',
-    external: ['vscode'],
+    external: ['vscode', 'better-sqlite3'],
     logLevel: 'silent',
     plugins: [
       /* add to the end of plugins array */
